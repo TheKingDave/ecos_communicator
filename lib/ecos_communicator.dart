508115bffb9481f88e4f8e271b3fe59785378dc0
TheKingDave <david@langheiter.com>
2020-10-24 18:49:43 +0200

    added export of parameter

--- lib/ecos_communicator.dart
+++ lib/ecos_communicator.dart
@@ -3,6 +3,4 @@
 /// More dartdocs go here.
 library ecos_communicator;
 
-export 'src/ecos_communicator_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/parameter.dart';
